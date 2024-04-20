c42875c0d07e49587a25578caaee6ec902e46d58
QiHe001 <167520283+QiHe001@users.noreply.github.com>
2024-04-20 20:21:36 +0800

    pull

--- src/main/java/com/qihe/tliseProject/denglu.java
+++ src/main/java/com/qihe/tliseProject/denglu.java
@@ -2,6 +2,7 @@ package com.qihe.tliseProject;
 
 public class denglu {
     public static void main(String[] args) {
+        System.out.println("push commit");
         System.out.println("pull commit");
     }
 }
